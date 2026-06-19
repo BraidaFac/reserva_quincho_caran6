55f2a79584b6471456ba387ffd234b2744ac6aed
BraidaFac <facundobraida98@gmail.com>
2026-06-19 16:43:30 +1200

    feat: register sw.js on client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Toaster } from "sonner";
-
+import SWRegister from "./sw-register";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -34,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body suppressHydrationWarning>
+        <SWRegister />
         {children}
         <Toaster richColors position="top-right" />
       </body>
